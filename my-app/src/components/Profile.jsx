575d438b2230a5cb19ac5317cdce130bbce52870
KennyBuzz <dshpityuk@gmail.com>
2021-03-07 16:50:11 +0300

    first-commit

---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+        <img src='https://static.addtoany.com/images/dracaena-cinnabari.jpg' alt='Profile'/>
+        </div>
+    <div>ava+descriptions</div>
+    <div>My Posts</div>
+    <div>New posts</div>
+    <div>Post 1</div>
+    <div>Post 2</div>
+</div>
+
+}
+
+export default Profile;
